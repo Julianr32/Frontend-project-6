d18053136f90b4bafcd2435b8b52dc9f6bb7d00d
Julian Rebolledo <rebolledojulian96@gmail.com>
2020-09-30 22:32:38 -0300

    First commit

--- js/main.js
+++ js/main.js
@@ -14,27 +14,23 @@ menuClose.addEventListener("click", () => {
 
 /* ====SLIDER==== */
 //Set Content.
-const slides = [
-  {
+const slides = [{
     id: 1,
     img: "images/desktop-image-hero-1.jpg",
     title: "Discover innovative ways to decorate",
-    info:
-      "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form andnfunction in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    info: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form andnfunction in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
   },
   {
     id: 2,
     img: "images/desktop-image-hero-2.jpg",
     title: "We are available all across the globe",
-    info:
-      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    info: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
   },
   {
     id: 3,
     img: "images/desktop-image-hero-3.jpg",
     title: "Manufactured with the best materials",
-    info:
-      "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    info: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
   },
 ];
 
@@ -51,7 +47,7 @@ window.addEventListener("DOMContentLoaded", () => {
   showSlide();
 });
 
-//Function that shows the slide based on the 'currenSlide' variable value.
+//Function that shows the slide based on the 'currentSlide' variable value.
 function showSlide() {
   const slide = slides[currentSlide];
   slideImg.src = slide.img;
